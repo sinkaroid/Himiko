72f4c501a3799f3f062ecc2115c11d98c8899fa2
sinkaroid <bibsindra@gmail.com>
2019-08-06 02:06:01 +0700

    cat

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ mass yande.re stealer
 ![](https://1.bp.blogspot.com/-fIfEmdtv-GI/XUdOAc8Et4I/AAAAAAAAJmU/InyiZ1lRIHsZ7pGhBnjapfTt5BG_FBVEACEwYBhgL/s1600/Screenshot_56.png)  
 
 ```sh
-# post?tags=hews; ur fav artist put here
-$ ./yandere.sh
-```
+$ cat ${name}.html | grep img | grep -Po 'largeimg" href="\K.*?(?=")' | sed 's/\?.*//' > result/links.txt;
+```  
+or  
+```sh
+$ ./yandere.sh #chmod +x if necessary
+```  
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- yandere.sh
+++ yandere.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#sin
+
+#view
 RED=$(tput setaf 1) 
 GREEN=$(tput setaf 2) 
 CYAN=$(tput setaf 6) 
 WHITE=$(tput setaf 7) 
+MAGENTA=$(tput setaf 5) 
 
 begin=$(date +"%s")
 
@@ -16,17 +20,25 @@ __ ((_)\ _ )\ _((_|_))_ ((_)(_))((_)
  \ V / / _ \ | .' || |) | _||   / _|  
   |_| /_/ \_\|_|\_||___/|___|_|_\___|                                                  
 "
-echo -e "${WHITE}"
-wget -O memek.html https://yande.re/post?tags=hews; #ur fav artist
+echo "YANDE.RE STEALER"
+
+#get
+read -p "${WHITE}artist/tags: " name
+echo -e "\n"
+read -p "${MAGENTA}${name} Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo -e "reading source for /${GREEN}$name ..
+${WHITE}"
+
+wget -O ${name}.html https://yande.re/post?tags=${name};
 
 #do
 mkdir result;
-cat memek.html | grep img | grep -Po 'largeimg" href="\K.*?(?=")' | sed 's/\?.*//' > result/links.txt; 
+cat ${name}.html | grep img | grep -Po 'largeimg" href="\K.*?(?=")' | sed 's/\?.*//' > result/links.txt; 
 cd result;
 wget -i links.txt
 
 #length
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
-echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed ${RED}/yandere.sh"
+echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed ${RED}yande.re stealer"
 
